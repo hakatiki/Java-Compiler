386f15707e62bdc66f1be88f0549dae1d15e3180
Bálint <takats>
2021-06-21 18:22:33 +0200

    + Addexpr

--- pp-project/src/Generation/Generator.java
+++ pp-project/src/Generation/Generator.java
@@ -13,6 +13,7 @@ import java.util.List;
 // TODO :: visitWhileLoop lengthStat+1???????
 // TODO Check if continue scope is everywhere!
 // TODO Dont forget to add registers to regs ParseTreeProperty
+// HARDCODED 4 in visitGetIndex
 
 public class Generator extends GrammarBaseVisitor<List<String>> {
 
@@ -56,13 +57,31 @@ public class Generator extends GrammarBaseVisitor<List<String>> {
 
     @Override public List<String>  visitNotExpr(GrammarParser.NotExprContext ctx) { return visitChildren(ctx); }
 
-    @Override public List<String>  visitArrayExpr(GrammarParser.ArrayExprContext ctx) { return visitChildren(ctx); }
 
     @Override public List<String>  visitGetThreadId(GrammarParser.GetThreadIdContext ctx) { return visitChildren(ctx); }
 
-    @Override public List<String>  visitGetIndex(GrammarParser.GetIndexContext ctx) { return visitChildren(ctx); }
 
-    @Override public List<String>  visitAddExpr(GrammarParser.AddExprContext ctx) { return visitChildren(ctx); }
+    @Override public List<String>  visitAddExpr(GrammarParser.AddExprContext ctx) {
+        continueScope(ctx);
+        List<String> current = new LinkedList<>();
+        List<String> lhs  = visit(ctx.expr(0));
+        String reg0 = regs.get(ctx.expr(0));
+        List<String> rhs  = visit(ctx.expr(1));
+        String reg1 = regs.get(ctx.expr(1));
+        // Idk about this is stack and memory different????? if yes this is good
+        // If they are the same then it will mess up scopes...
+        String save = "Push "+ reg0;
+        String reg2 = reg1.equals(reg0)?(reg0.equals("1")?"2":"1"):reg0;
+        String get = "Pop "+ reg2;
+        String addInstr = "Compute Add "+reg2+ " " + reg1 + " " + reg0;
+        current.addAll(lhs);
+        current.add(save);
+        current.addAll(rhs);
+        current.add(get);
+        current.add(addInstr);
+        regs.put(ctx,reg0);
+        return current;
+    }
 
     @Override public List<String>  visitCompExpr(GrammarParser.CompExprContext ctx) { return visitChildren(ctx); }
 
@@ -92,19 +111,35 @@ public class Generator extends GrammarBaseVisitor<List<String>> {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    // definitely not correct... :(
+    @Override public List<String>  visitArrayExpr(GrammarParser.ArrayExprContext ctx) {
+        continueScope(ctx);
+        regs.put(ctx, regs.get(ctx.arr()));
+        return visit(ctx.arr());
+    }
     @Override public List<String>  visitParExpr(GrammarParser.ParExprContext ctx) {
-        List<String> current = new LinkedList<>();
+        continueScope(ctx);
         List<String> exprCode = visit(ctx.expr());
         String reg = regs.get(ctx.expr());
-
-        String store = "Store "+ reg.toString() +" (DirAddr " + " " + " )";
-        current.addAll(exprCode);
-        current.add(store);
+        regs.put(ctx, reg);
+        return exprCode;
+    }
+    @Override public List<String>  visitGetIndex(GrammarParser.GetIndexContext ctx) {
+        continueScope(ctx);
+        List<String> current =  new LinkedList<>();
+        Scope currScope = scope.get(ctx);
+        int addressA = currScope.address(ctx.ID().getText());
+        List<String> exprCode = visit(ctx.expr());
+        String reg0 = regs.get(ctx.expr());
+        String reg1 = reg0.equals("1")? "2":"1";
+        String loadFour = "Load (ImmValue 4) "+reg1+"";
+        String offsetComp = "Compute Mul " +reg0+" " + reg1+" " + reg0;
+        String loadInstr = "Load ( DirAddr " + reg0 +" ) " + reg0;
+        current.add(loadFour);
+        current.add(offsetComp);
+        current.add(loadInstr);
+        regs.put(ctx, reg0);
         return current;
     }
-
     @Override public List<String>  visitIfStatement(GrammarParser.IfStatementContext ctx) {
         continueScope(ctx);
         List<String> current = new LinkedList<>();
