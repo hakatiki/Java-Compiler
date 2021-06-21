bce51842e95dd358e06bbb249edbe21a7e07570f
Im_Cynx <silentninj4.7@gmail.com>
2021-06-21 09:30:16 +0200

    + potential setup for code generation

---
+++ pp-project/src/Generation/Generator.java
@@ -0,0 +1,20 @@
+package Generation;
+
+import ANTLR.GrammarBaseVisitor;
+
+public class Generator extends GrammarBaseVisitor {
+
+    // We need to model the SprIl places?
+    // for the ILOC example they had registers, but yeah idk
+
+//    public Program generate(ParseTree tree, Result checkResult) {
+//        this.prog = new Program();
+//        this.checkResult = checkResult;
+//        this.regs = new ParseTreeProperty<>();
+//        this.labels = new ParseTreeProperty<>();
+//        this.regCount = 0;
+//        tree.accept(this);
+//        return this.prog;
+//    }
+
+}
