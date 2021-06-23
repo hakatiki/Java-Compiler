4a2b37c3f74c2b1bb454053ec28b38ceb9d9aaed
Im_Cynx <silentninj4.7@gmail.com>
2021-06-23 11:45:26 +0200

    + test file in Sprockell folder

--- pp-project/src/sprockell-master/src/test.hs
+++ pp-project/src/sprockell-master/src/test.hs
@@ -1,17 +1,22 @@
 import Sprockell
 prog :: [Instruction]
 prog = [
-    Load (ImmValue 0) regA,
-    Store regA (DirAddr 0),
-    Load (DirAddr 0) regA,
+    Load (ImmValue 0 ) regA,
+    Store regA (DirAddr 0 ),
+    Load (DirAddr 0 ) regA,
     Push regA,
-    Load (ImmValue 3) regA,
+    Load (ImmValue 3 ) regA,
     Pop regB,
     Compute Gt regB regA regA,
-    Branch regA (Rel 4),
-    Load (DirAddr 0) regA,
+    Branch regA (Rel 9),
+    Load (DirAddr 0 ) regA,
+    Push regA,
+    Load (ImmValue 1 ) regA,
+    Pop regB,
+    Compute Add regB regA regA,
     WriteInstr regA numberIO,
-    Jump (Rel ( -8)),
+    Store regA (DirAddr 0 ),
+    Jump (Rel ( -13)),
     EndProg
     ]
 main = run [prog]
